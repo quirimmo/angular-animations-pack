2fc64fd5cab8d57af21eac0efd97e8debc951058
Quirino Lo Russo <quirimmo@gmail.com>
2018-07-29 21:15:10 +0200

    working on unit tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from "@angular/core";
-import { Hero } from "./hero-list-basic/hero.service";
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero-list-basic/hero.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = "app";
+export class AppComponent implements OnInit {
+  title = 'app';
   heroes: Hero[] = [];
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { HeroListBasicComponent } from './hero-list-basic/hero-list-basic.component';
-import { AngularAnimationsPackModule } from "./angular-animations-pack/angular-animations-pack.module";
+import { AngularAnimationsPackModule } from './angular-animations-pack/angular-animations-pack.module';
 
 @NgModule({
   declarations: [AppComponent, HeroComponent, HeroListBasicComponent],
